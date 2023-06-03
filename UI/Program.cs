5e907bab307cfda6fd0a430c3fb05a15287fbba0
Kazım Kesler <Kazm Kesler@LAPTOP-7QDBUL4J>
2023-06-04 01:41:42 +0300

    add an example

--- UI/Program.cs
+++ UI/Program.cs
@@ -8,6 +8,11 @@ namespace UI
 {
     internal class Program
     {
+        static Result check()
+        {
+            Console.WriteLine("Run");
+            return Result.OK("run");
+        }
         static void Main(string[] args)
         {
             //Non-Generic(with null data property)
@@ -16,7 +21,7 @@ namespace UI
             bool boolean = Result.OK();
             bool boolean1 = !Result.NotFound();
             Result and1 = Result.NotFound() && Result.BadRequest(); //return first wrong object, short-circuit
-            Result and2 = Result.NotFound() && Result.OK();//return first wrong object, short-circuit
+            Result and2 = Result.NotFound() && check();//return first wrong object, short-circuit
             Result and3 = Result.OK() && Result.NotFound();//return first wrong object, short-circuit
             Result and4 = Result.OK() && Result.Created();//return Success object, short-circuit
 
